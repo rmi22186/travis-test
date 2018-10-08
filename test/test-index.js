0984aa7c0a8da414ff39778c5e7c8f2878c07a75
Robert Ing <ring@mparticle.com>
2018-10-08 10:36:40 -0400

    test travis

---
+++ test/test-index.js
@@ -0,0 +1,27 @@
+window.MockHttpServer = require('./mockhttprequest.js');
+window.Should = require('should');
+
+var mp = function () {
+    var self = this;
+
+    this.addForwarder = function (forwarder) {
+        self.forwarder = new forwarder.constructor();
+    };
+
+    this.getCurrentUser = function() {
+        return currentUser();
+    };
+};
+
+function currentUser() {
+    return {
+        getMPID: function() {
+            return 123;
+        }
+    };
+}
+
+window.mParticle = new mp();
+
+require('../LeanplumAnalyticsEventForwarder.js');
+require('./tests.js');
