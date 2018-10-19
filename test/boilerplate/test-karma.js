0a30fb407afba43e6fb73ee0798d6012bc4d16f8
Robert Ing <ring@mparticle.com>
2018-10-19 11:56:01 -0400

    remove old example, move karma

--- test/boilerplate/test-karma.js
+++ test/boilerplate/test-karma.js
@@ -3,6 +3,6 @@ var util = require('util');
 var defaultArgs = '--single-run --browsers=Safari,ChromeHeadless,FirefoxHeadless';
 var args = process.argv.slice(2).join(' ') || defaultArgs;
 
-var testCommand = util.format('browserify test/boilerplate/test-index.js -o test/test-bundle.js && karma start %s test/karma.config.js', args);
+var testCommand = util.format('browserify test/boilerplate/test-index.js -o test/test-bundle.js && karma start %s test/boilerplate/karma.config.js', args);
 console.log('Running command: \n' + testCommand);
 shell.exec(testCommand);
